573b1a0e77c57ed4ae7cb19d6f964b1531f62a1d
Ural Davletshin <ural.davletshin@gmail.com>
2017-08-31 00:13:39 +0300

    Reorgonize pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,62 +1,25 @@
 'use strict'
 
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var sourcemaps = require('gulp-sourcemaps');
-var del = require('del');
-var less = require('gulp-less');
-var sass = require('gulp-sass');
-var path = require('path');
-var pug = require('gulp-pug');
-var webserver = require('gulp-webserver');
+var gulp = require('gulp'),
+    scss = require('gulp-sass'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    pug = require('gulp-pug');
 
-var paths = {
-    bootstrap_css: ['node_modules/bootstrap/dist/css/bootstrap.css'],
-    less: ['assets/less/*.less'],
-    views: ['src/*/*.pug'],
-    includes: ['src/*/*/*.pug'],
-    scss: ['src/*/*/*.scss'],
-    dist: ['./build']
+var sources = {
+    bootstrap: ['node_modules/bootstrap/dist/css/bootstrap.min.css']
+};
+
+var dest = {
+    css: './build/css'
 };
 
 gulp.task('bootstrap_css', function() {
-    return gulp.src(paths.bootstrap_css)
+    return gulp.src(sources.bootstrap)
         .pipe(sourcemaps.init())
         .pipe(concat('bootstrap.css'))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(`${paths.dist}/css`));
+        .pipe(gulp.dest(dest.css));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.bootstrap_css, ['bootstrap_css']);
-    gulp.watch(paths.less, ['less']);
-    gulp.watch(paths.views, ['views']);
-    gulp.watch(paths.includes, ['views']);
-    gulp.watch(paths.scss, ['scss']);
-});
-
-gulp.task('views', function () {
-    return gulp.src(paths.views)
-        .pipe(pug({
-            pretty: true,
-        }))
-        .pipe(gulp.dest(`${paths.dist}/`))
-});
-
-gulp.task('scss', function () {
-    gulp.src('./src/assets/index.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest(`${paths.dist}/css`))
-})
-
-gulp.task('webserver', function() {
-    gulp.src('./build/')
-        .pipe(webserver({
-            livereload: true,
-            path: '/',
-            port: 8088,
-            fallback: 'index.html'
-        }));
-});
-
-gulp.task('default', ['watch', 'views', 'bootstrap_css', 'scss', 'webserver']);
+gulp.task('build', ['bootstrap_css']);
